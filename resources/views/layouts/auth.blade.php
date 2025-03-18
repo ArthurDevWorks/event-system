32c1d53e3b52d8cfb37566a90cef6c3ffe96e97e
Arthur Soares Parente <arthur.parente@tridia.com.br>
2025-03-18 15:11:17 -0300

    :ok_hand: style: Ajuste para retirar placehoders desnecessários além de separar arquivos scss.

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -5,7 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-  @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+  @vite(['resources/sass/auth.scss', 'resources/js/app.js'])
 
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 
